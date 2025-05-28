8c903997f4552679d15fb2c6dae50676a85e3923
Ali Deniz Yakar <denizyakar00@gmail.com>
2025-05-28 04:14:12 +0300

    updated

---
+++ UIKitHomework/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  UIKitHomework
+//
+//  Created by Ali Deniz Yakar on 22.05.2025.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
